af08907aeb08a2136d13e703c78d72864e180832
nferrari3444 <nferrari3444@gmail.com>
2024-03-05 16:14:30 -0300

    add InventarioProject to static_root

--- InventarioProject/InventarioProject/settings/production.py
+++ InventarioProject/InventarioProject/settings/production.py
@@ -44,7 +44,7 @@ USE_X_FORWARDED_HOST=True
 
 STATICFILES_DIRS =  os.path.join(BASE_DIR, "static/"),
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'InventarioProject/staticfiles')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
