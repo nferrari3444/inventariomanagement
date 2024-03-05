3ff561c836a4444a3661f2b40e741ba3fcf73f68
nferrari3444 <nferrari3444@gmail.com>
2024-03-05 19:18:53 -0300

    ADD STATICFILES_FINDES production.py

--- InventarioProject/InventarioProject/settings/production.py
+++ InventarioProject/InventarioProject/settings/production.py
@@ -56,6 +56,13 @@ STATICFILES_DIRS =  [os.path.join(BASE_DIR, "static")]
 
 print('STATICFILES_DIRS is ', STATICFILES_DIRS )
 
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    #"staticfiles.finders.FileSystemFinder",
+    #"staticfiles.finders.AppDirectoriesFinder",
+    "compressor.finders.CompressorFinder",
+)
 STORAGES = {
     # ...
     "staticfiles": {
