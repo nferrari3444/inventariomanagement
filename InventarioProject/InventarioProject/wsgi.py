9693b3ea230dbaa471d468f3b808b0cd8cf75c6a
nferrari3444 <nferrari3444@gmail.com>
2024-03-05 07:39:45 -0300

    add production in wsgi file

--- InventarioProject/InventarioProject/wsgi.py
+++ InventarioProject/InventarioProject/wsgi.py
@@ -13,7 +13,7 @@ from django.core.wsgi import get_wsgi_application
 
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InventarioProject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InventarioProject.settings.production')
     
 application = get_wsgi_application()
 
