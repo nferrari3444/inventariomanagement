937a15e1c24ae6f99abec7929d5888cd0e08826a
nferrari3444 <nferrari3444@gmail.com>
2024-03-05 08:35:47 -0300

    remove STATICFILES_STORAGE

--- InventarioProject/InventarioProject/settings/production.py
+++ InventarioProject/InventarioProject/settings/production.py
@@ -11,7 +11,7 @@ COMPRESS_ENABLED = True
 STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 
 
-STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS =  [os.path.join(BASE_DIR, "static")]
 
 STATIC_ROOT =  os.path.join(BASE_DIR, "staticfiles_build")
