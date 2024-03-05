ae287135ef251495792355789cf5d788dbf7977f
nferrari3444 <nferrari3444@gmail.com>
2024-03-05 15:18:26 -0300

    change vercel_json

--- InventarioProject/InventarioProject/settings/production.py
+++ InventarioProject/InventarioProject/settings/production.py
@@ -43,7 +43,8 @@ USE_X_FORWARDED_HOST=True
 # AWS_S3_SECURE_URLS= True
 
 STATICFILES_DIRS =  os.path.join(BASE_DIR, "static"),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URLS ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
