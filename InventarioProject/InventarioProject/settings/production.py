87c4045baa3e08d5acffddb7ab0aa549c6fb00fd
nferrari3444 <nferrari3444@gmail.com>
2024-03-05 09:27:28 -0300

    add django.contrib.staticfiles to FINDERS

--- InventarioProject/InventarioProject/settings/production.py
+++ InventarioProject/InventarioProject/settings/production.py
@@ -8,14 +8,18 @@ COMPRESS_URL = '/static/'
 COMPRESS_ROOT = BASE_DIR / 'static'
 
 COMPRESS_ENABLED = True
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 
 
-STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_DIRS =  [os.path.join(BASE_DIR, "static")]
 
 STATIC_ROOT =  os.path.join(BASE_DIR, "staticfiles_build","static")
 
+STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',
+                        "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder")
 
 
 DATABASES = {
