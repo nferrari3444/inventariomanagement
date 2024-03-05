47b088f93d3bf193c8275054f60dee4fa705afb3
nferrari3444 <nferrari3444@gmail.com>
2024-03-05 13:01:56 -0300

    add STATICFILES_FINDERS

--- InventarioProject/InventarioProject/settings/production.py
+++ InventarioProject/InventarioProject/settings/production.py
@@ -4,11 +4,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 USE_X_FORWARDED_HOST=True
 
-# COMPRESS_URL = '/static/'
-# COMPRESS_ROOT = BASE_DIR / 'static'
+COMPRESS_URL = '/static/'
+COMPRESS_ROOT = BASE_DIR / 'static'
 
-# COMPRESS_ENABLED = True
+COMPRESS_ENABLED = True
 
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',
+                       )
 
 AWS_S3_OBJECT_PARAMETERS = {
     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
@@ -39,8 +42,6 @@ DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
 
 #STATICFILES_STORAGE= 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-#STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',
-#                       )
 
 #STATICFILES_STORAGE='custom_storages.StaticStorage'
 
