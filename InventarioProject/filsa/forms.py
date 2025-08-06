7500d187a9d3715b0cb1b2fd3d8927bd53e3afd9
nferrari3444 <nferrari3444@gmail.com>
2025-08-05 23:49:20 -0300

    se borran lineas comentadas

--- InventarioProject/filsa/forms.py
+++ InventarioProject/filsa/forms.py
@@ -400,19 +400,12 @@ class OutboundOrderForm(forms.ModelForm):
                                      }), empty_label='-------------', to_field_name='name')
     
     
-    # receptor = ModelChoiceField(queryset=CustomUser.objects.all()
-    #                                    ,widget=forms.Select(attrs={
-    #                                       'class': "bg-white-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500",
-    #                                       'style': 'max-width: auto;',
-    #                                   }), empty_label='-------------', to_field_name=  'username')
-    
+   
     observationsSolicitud = forms.CharField(required=False, widget=forms.Textarea(attrs={'rows':4, 'class': "block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" , 'placeholder': "Observaciones..."}))
 
     class Meta:
         model = Tasks
-        # fields = ['product','department','issuer', 'motivoEgreso', 'cantidad', 'date']
-        #fields = ['department','issuer', 'motivoEgreso', 'warehouse','date', 'receptor' ]
-       # fields = ['warehouse','motivoEgreso', 'issuer', 'receptor', 'department' , 'date']
+       
         fields = ['motivoEgreso', 'issuer', 'receptor', 'department' , 'date']
     
     def __init__(self, *args, **kwargs):
