bcc3c8d86335e83debc4a835c8776930a494f2a6
nferrari3444 <nferrari3444@gmail.com>
2026-01-05 11:57:39 -0300

    change settings to production in wsgi.py file

--- InventarioProject/InventarioProject/wsgi.py
+++ InventarioProject/InventarioProject/wsgi.py
@@ -14,9 +14,9 @@ from django.core.wsgi import get_wsgi_application
 
 
 #os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InventarioProject.settings.local')
-settings_module = 'InventarioProject.settings.local' if 'runserver' in sys.argv else 'InventarioProject.settings.production'
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
-#os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InventarioProject.settings.production')
+# settings_module = 'InventarioProject.settings.local' if 'runserver' in sys.argv else 'InventarioProject.settings.production'
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InventarioProject.settings.production')
 
 
 application = get_wsgi_application()
