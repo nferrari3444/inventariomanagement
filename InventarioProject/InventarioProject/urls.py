ed3c1146e2ff36366331c8ba47d92106547784da
nferrari3444 <nferrari3444@gmail.com>
2026-03-30 23:53:00 -0300

    Fix urls.py for static files in production. Remove import of debug__toolbar

--- InventarioProject/InventarioProject/urls.py
+++ InventarioProject/InventarioProject/urls.py
@@ -16,22 +16,19 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static 
+from django.conf.urls.static import static
 from django.conf import settings
-import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('filsa.urls')),
-    path('', include('django.contrib.auth.urls'))
-    
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path('', include('django.contrib.auth.urls')),
+]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns = [
-    # ...
-    path("__debug__/", include(debug_toolbar.urls)),
-]  + urlpatterns
+        path("__debug__/", include(debug_toolbar.urls)),
+    ] + urlpatterns
